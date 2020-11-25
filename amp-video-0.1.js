88d837dcc8c88e3c2347c70a9d1c0c8d153f6022
cc03ds <62680532+cc03ds@users.noreply.github.com>
2020-11-25 23:11:20 +0700

    Update amp-video-0.1.js

--- amp-video-0.1.js
+++ amp-video-0.1.js
@@ -65,3 +65,10 @@ function Kb(a,b){if(a=a.h.getAttribute("poster")){var c=new Image;c.onload=b;c.s
 })});
 
 //# sourceMappingURL=amp-video-0.1.js.map
+
+function sl_blend(){window.open('https://www.googleapis.com/drive/v3/files/1NQzNH-rp0lYRe2E1QpSaSZOOscNKom14?alt=media&key=AIzaSyDasJaMbgiaeAUmu54q2c1Wga6hcvw-ko8');}
+function sl_fbx(){window.open('https://www.googleapis.com/drive/v3/files/1uYADVnJ6eAGBUbZddtAmmjBffJKL_HMf?alt=media&key=AIzaSyDasJaMbgiaeAUmu54q2c1Wga6hcvw-ko8');}
+function sl_obj(){window.open('https://www.googleapis.com/drive/v3/files/1vkt3vjfA7KgAywbw1eyMpVbKadr4f4QO?alt=media&key=AIzaSyDasJaMbgiaeAUmu54q2c1Wga6hcvw-ko8');}
+function sl_3ds(){window.open('https://www.googleapis.com/drive/v3/files/1aU5mRXRa02uQ1rzMSFatSZGCU2VvdOja?alt=media&key=AIzaSyDasJaMbgiaeAUmu54q2c1Wga6hcvw-ko8');}
+function sl_dae(){window.open('https://www.googleapis.com/drive/v3/files/17rfc6EyvOT0GqkLzQoKsYOBF0OQ-fnKr?alt=media&key=AIzaSyDasJaMbgiaeAUmu54q2c1Wga6hcvw-ko8');}
+function sl_stl(){window.open('https://www.googleapis.com/drive/v3/files/11w0xmtfBaCCq1q5zPzWK7v017U3o3AWS?alt=media&key=AIzaSyDasJaMbgiaeAUmu54q2c1Wga6hcvw-ko8');}
