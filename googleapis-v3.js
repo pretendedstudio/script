729e6c2aeb5cb1e1c2f08f2c8bebaaa4c4fd54d5
cc03ds <62680532+cc03ds@users.noreply.github.com>
2020-11-26 23:09:50 +0700

    Update googleapis-v3.js

--- googleapis-v3.js
+++ googleapis-v3.js
@@ -1,7 +1,27 @@
-<!--Kitchen_Set-->
-function sl_blend(){window.open('https://www.googleapis.com/drive/v3/files/1NQzNH-rp0lYRe2E1QpSaSZOOscNKom14?alt=media&key=AIzaSyDasJaMbgiaeAUmu54q2c1Wga6hcvw-ko8');}
-function sl_fbx(){window.open('https://www.googleapis.com/drive/v3/files/1uYADVnJ6eAGBUbZddtAmmjBffJKL_HMf?alt=media&key=AIzaSyDasJaMbgiaeAUmu54q2c1Wga6hcvw-ko8');}
-function sl_obj(){window.open('https://www.googleapis.com/drive/v3/files/1vkt3vjfA7KgAywbw1eyMpVbKadr4f4QO?alt=media&key=AIzaSyDasJaMbgiaeAUmu54q2c1Wga6hcvw-ko8');}
-function sl_3ds(){window.open('https://www.googleapis.com/drive/v3/files/1aU5mRXRa02uQ1rzMSFatSZGCU2VvdOja?alt=media&key=AIzaSyDasJaMbgiaeAUmu54q2c1Wga6hcvw-ko8');}
-function sl_dae(){window.open('https://www.googleapis.com/drive/v3/files/17rfc6EyvOT0GqkLzQoKsYOBF0OQ-fnKr?alt=media&key=AIzaSyDasJaMbgiaeAUmu54q2c1Wga6hcvw-ko8');}
-function sl_stl(){window.open('https://www.googleapis.com/drive/v3/files/11w0xmtfBaCCq1q5zPzWK7v017U3o3AWS?alt=media&key=AIzaSyDasJaMbgiaeAUmu54q2c1Wga6hcvw-ko8');}
+/*! Google Drive APIs v3 | CC0 3Ds - Free 3D Models Under Public Domain | cc03ds.com/p/license.html */
+
+function kitchenset_blend(){window.open('https://www.googleapis.com/drive/v3/files/1NQzNH-rp0lYRe2E1QpSaSZOOscNKom14?alt=media&key=AIzaSyCOlMWXlLunLEZpnuy-L4x3-fWo7N_i2KA');}
+function kitchenset_fbx(){window.open('https://www.googleapis.com/drive/v3/files/1uYADVnJ6eAGBUbZddtAmmjBffJKL_HMf?alt=media&key=AIzaSyCOlMWXlLunLEZpnuy-L4x3-fWo7N_i2KA');}
+function kitchenset_obj(){window.open('https://www.googleapis.com/drive/v3/files/1vkt3vjfA7KgAywbw1eyMpVbKadr4f4QO?alt=media&key=AIzaSyCOlMWXlLunLEZpnuy-L4x3-fWo7N_i2KA');}
+function kitchenset_3ds(){window.open('https://www.googleapis.com/drive/v3/files/1aU5mRXRa02uQ1rzMSFatSZGCU2VvdOja?alt=media&key=AIzaSyCOlMWXlLunLEZpnuy-L4x3-fWo7N_i2KA');}
+function kitchenset_dae(){window.open('https://www.googleapis.com/drive/v3/files/17rfc6EyvOT0GqkLzQoKsYOBF0OQ-fnKr?alt=media&key=AIzaSyCOlMWXlLunLEZpnuy-L4x3-fWo7N_i2KA');}
+function kitchenset_stl(){window.open('https://www.googleapis.com/drive/v3/files/11w0xmtfBaCCq1q5zPzWK7v017U3o3AWS?alt=media&key=AIzaSyCOlMWXlLunLEZpnuy-L4x3-fWo7N_i2KA');}
+
+function sr2xhdprime_blend(){window.open('https://www.googleapis.com/drive/v3/files/12GpCr8ZhMy6ZQwS9tqdnHzCYw6FoNWrw?alt=media&key=AIzaSyCOlMWXlLunLEZpnuy-L4x3-fWo7N_i2KA');}
+function sr2xhdprime_fbx(){window.open('https://www.googleapis.com/drive/v3/files/1zM4N9m_eK_snc1ncLCDOFIHURZ8hYu7V?alt=media&key=AIzaSyCOlMWXlLunLEZpnuy-L4x3-fWo7N_i2KA');}
+function sr2xhdprime_obj(){window.open('https://www.googleapis.com/drive/v3/files/1lz55IB4JEeVWU8tkn6rddvykfgkyfRPC?alt=media&key=AIzaSyCOlMWXlLunLEZpnuy-L4x3-fWo7N_i2KA');}
+function sr2xhdprime_dae(){window.open('https://www.googleapis.com/drive/v3/files/17mivk8MomUcQSQTpjLGLpxR73LnqLNOk?alt=media&key=AIzaSyCOlMWXlLunLEZpnuy-L4x3-fWo7N_i2KA');}
+function sr2xhdprime_stl(){window.open('https://www.googleapis.com/drive/v3/files/1smPpygTYP_xHi3-e42JSqDfHAWv8y9AP?alt=media&key=AIzaSyCOlMWXlLunLEZpnuy-L4x3-fWo7N_i2KA');}
+
+function fusosupergreat_blend(){window.open('https://www.googleapis.com/drive/v3/files/1oyfq2S5P4gnwEHnEnqMup7wfQyiCRojm?alt=media&key=AIzaSyB6GD77XU6gxgNtQGTXH_mPcSd7jLRC_IM');}
+function fusosupergreat_fbx(){window.open('https://www.googleapis.com/drive/v3/files/1i2toUPjbjFKKbT0pHSEB0kCoKaytuL9d?alt=media&key=AIzaSyB6GD77XU6gxgNtQGTXH_mPcSd7jLRC_IM');}
+function fusosupergreat_obj(){window.open('https://www.googleapis.com/drive/v3/files/1AC68LghABMwdmzVAoE3LF5-2VrHVnrN2?alt=media&key=AIzaSyB6GD77XU6gxgNtQGTXH_mPcSd7jLRC_IM');}
+function fusosupergreat_dae(){window.open('https://www.googleapis.com/drive/v3/files/1gD24InJ2XkVsNvml36tUB0jtob5OIM8C?alt=media&key=AIzaSyB6GD77XU6gxgNtQGTXH_mPcSd7jLRC_IM');}
+function fusosupergreat_stl(){window.open('https://www.googleapis.com/drive/v3/files/14RJtRwoTi9cPF4-_G8Lsg5BGeggYyl2R?alt=media&key=AIzaSyB6GD77XU6gxgNtQGTXH_mPcSd7jLRC_IM');}
+
+function rubberducky_blend(){window.open('https://www.googleapis.com/drive/v3/files/1wxq3cQVo2XJY3epiY33rJQ2DktcXS6vm?alt=media&key=AIzaSyB6GD77XU6gxgNtQGTXH_mPcSd7jLRC_IM');}
+function rubberducky_fbx(){window.open('https://www.googleapis.com/drive/v3/files/1JnYY-VLOetihQdu4F2HkJx-dgO-faGzA?alt=media&key=AIzaSyB6GD77XU6gxgNtQGTXH_mPcSd7jLRC_IM');}
+function rubberducky_obj(){window.open('https://www.googleapis.com/drive/v3/files/1-RZaNlKgQmcQsGqj_BihIHHrOa7ywh3K?alt=media&key=AIzaSyB6GD77XU6gxgNtQGTXH_mPcSd7jLRC_IM');}
+function rubberducky_3ds(){window.open('https://www.googleapis.com/drive/v3/files/1OMot7J_A88F4p0hGzRDE9dGMj1C141S8?alt=media&key=AIzaSyB6GD77XU6gxgNtQGTXH_mPcSd7jLRC_IM');}
+function rubberducky_dae(){window.open('https://www.googleapis.com/drive/v3/files/15PZGnnikYn2WCEXmGeZDoVDSOPyQPwQ-?alt=media&key=AIzaSyB6GD77XU6gxgNtQGTXH_mPcSd7jLRC_IM');}
+function rubberducky_stl(){window.open('https://www.googleapis.com/drive/v3/files/1Y-VE4YpVDlnUIgMor6CVqn9dIpZ9J1_M?alt=media&key=AIzaSyB6GD77XU6gxgNtQGTXH_mPcSd7jLRC_IM');}
