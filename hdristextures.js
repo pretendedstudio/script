796a724c70a23d5c7deb000cd3f529c589a5f2c1
cc03ds <62680532+cc03ds@users.noreply.github.com>
2020-11-29 09:52:53 +0700

    Create hdristextures.js

---
+++ hdristextures.js
@@ -0,0 +1,8 @@
+function tiergarten_hdri(){window.open('https://hdrihaven.com/hdri/?h=tiergarten');}
+function urbanstreet04_hdri(){window.open('https://hdrihaven.com/hdri/?h=urban_street_04');}
+
+function wood032_texture(){window.open('https://cc0textures.com/view?id=Wood032');}
+function fabric004_texture(){window.open('https://cc0textures.com/view?id=Fabric004');}
+
+function _hdri(){window.open('');}
+function _texture(){window.open('');}
