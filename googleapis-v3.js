44ad865ce80001bfe609862c9f1cebab0e7fd1b8
cc03ds <62680532+cc03ds@users.noreply.github.com>
2020-12-01 02:24:23 +0700

    Update googleapis-v3.js

--- googleapis-v3.js
+++ googleapis-v3.js
@@ -700,5 +700,6 @@ function sterkspruitfalls_hdri(){window.open('https://hdrihaven.com/hdri/?h=ster
 function sabietent_hdri(){window.open('https://hdrihaven.com/hdri/?h=sabie_tent');}, 
 function _hdri(){window.open('https://hdrihaven.com/hdri/?h=');}
 /*! Google Drive APIs v3 | CC0 Textures - Free Public Domain PBR Materials | https://help.cc0textures.com/doku.php?id=website:license */
-function wood032_texture(){window.open('https://cc0textures.com/view?id=Wood032');}, function fabric004_texture(){window.open('https://cc0textures.com/view?id=Fabric004');}
+function wood032_texture(){window.open('https://cc0textures.com/view?id=Wood032');}
+function fabric004_texture(){window.open('https://cc0textures.com/view?id=Fabric004');}
 function _texture(){window.open('https://cc0textures.com/view?id=');}
