0aa1fc5c4f9c4fd0cd738daf9b67241246af35bb
cc03ds <62680532+cc03ds@users.noreply.github.com>
2020-12-01 02:30:42 +0700

    Create texture.js

---
+++ texture.js
@@ -0,0 +1,4 @@
+/*! Google Drive APIs v3 | CC0 Textures - Free Public Domain PBR Materials | https://help.cc0textures.com/doku.php?id=website:license */
+function wood032_texture(){window.open('https://cc0textures.com/view?id=Wood032');}
+function fabric004_texture(){window.open('https://cc0textures.com/view?id=Fabric004');}
+function _texture(){window.open('https://cc0textures.com/view?id=');}
