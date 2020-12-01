28a3ca236f0adf84bf9941586dc757cceceaa766
cc03ds <62680532+cc03ds@users.noreply.github.com>
2020-12-01 16:42:18 +0700

    Delete texture.js

--- texture.js
+++
@@ -1,3 +0,0 @@
-/*! Google Drive APIs v3 | CC0 Textures - Free Public Domain PBR Materials | https://help.cc0textures.com/doku.php?id=website:license */
-function wood032_texture(){window.open('https://cc0textures.com/view?id=Wood032');}
-function fabric004_texture(){window.open('https://cc0textures.com/view?id=Fabric004');}
